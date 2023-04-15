d509fd98c569df702d1853c2466c530ca6441b60
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-14 23:44:01 -0400

    type text and save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,11 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
+  var today = dayjs().format('dddd, MMMM D');
+  $("#currentDay").text(today);
   
   // var militaryTime = dayjs().format('H');
-  var militaryTime = 10;
+  var militaryTime = 11;
   console.log(militaryTime);
 
   var hour9 = $('#hour-9').data('hour');
@@ -42,15 +44,45 @@ $(function () {
   }
 
 
-  var saveBtnEl = document.querySelector("#hour-9");
+  var Btn9El = document.getElementById('btn9');
+  var Btn10El = document.getElementById("btn10");
+  var Btn11El = document.getElementById("btn11");
+  var Btn12El = document.getElementById("btn12");
+  var Btn13El = document.getElementById("btn13");
+  var Btn14El = document.getElementById("btn14");
+  var Btn15El = document.getElementById("#btn15");
+  var Btn16El = document.getElementById("#btn16");
+  var Btn17El = document.getElementById("#btn17");
 
-  saveBtnEl.addEventListener("click", function(){
-    console.log("button was pushed");
+  
+  var text10El = document.getElementById('text10');
+  var text11El = document.getElementById('text11');
+  var text12El = document.getElementById('text12');
+  var text13El = document.getElementById('text13');
+  var text14El = document.getElementById('text14');
+  var text15El = document.getElementById('text15');
+  var text16El = document.getElementById('text16');
+  var text17El = document.getElementById('text17');
+
+
+
+
+
+
+
+  Btn9El.addEventListener("click", function(event){
+    event.preventDefault();
+    var text9 = document.getElementById('text9').value;
+    console.log(text9);
+    localStorage.setItem("text9", JSON.stringify(text9));
   })
+
+  console.log(JSON.parse(localStorage.getItem("text9")));
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
 
 
 
