fa2b0d23ef8cbe49452698b009866be9bfb43c08
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-17 19:39:22 -0400

    updated comments, cleaned up code and wrote README file

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-# work-day-scheduler
+# Work Day Scheduler
 
-jquery
-bootstrap?
-already made code
-dayjs
+A Work Day Scheduler was created to provide the user with a site to save user input data (appointments) throughout a typical work day. Styling allows for easy recognition of the current time and events that occurred in the past or those still yet to come. The Work Day Scheduler was created with HTML, CSS and Javascript. 
+
+
+
+
+## Installation
+
+The application is accessible through any web browser at the following address:
+
+https://meghansimmons.github.io/work-day-scheduler/
+
+
+## Usage
+
+The website provides multiple time blocks for a user to enter data. The time blocks represent typical work day hours. The data can be saved and accessed at any time.
+
+
+## Contributing
+
+The current version of the Work Day Scheduler includes starter code given to the developer. A mockup of the finished product, requirements and preferences were also provided. In addition to the starter code, the following libraries and frameworks were utilized in the formatting of this website: Bootstrap, Font Awesome, Google Fonts, jQuery and Day.js. Any information about issues or bugs with the website are appreciated. 
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-  
+//the javascript code below enables the user to see the current date, add events to time-blocks and save their entries
+//the time-blocks themselves will be stylized to represent past, present and future time-blocks
+$(function () {
+
+  //use dayjs() to get and format today's date and append it to the website
   var today = dayjs().format('dddd, MMMM D');
   $("#currentDay").text(today);
   
-  var militaryTime = dayjs().format('H');
-  // var militaryTime = 11;
-  // console.log(militaryTime);
-
+//assign variables representing the hour from each time-block by its id name
   var hour9 = $('#hour-9').data('hour');
   var hour10 = $('#hour-10').data('hour');
   var hour11 = $('#hour-11').data('hour');
@@ -20,10 +17,20 @@
   var hour16 = $('#hour-16').data('hour');
   var hour17 = $('#hour-17').data('hour');
 
+  //create an array to hold hour values
+  //create an array to hold time-block id element names
   var hourArray = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
   var idArray = ["#hour-9","#hour-10", "#hour-11", "#hour-12","#hour-13","#hour-14","#hour-15","#hour-16","#hour-17"];
 
+  //format the current date and time data using dayjs() to give time in the 24-hour format
+  var militaryTime = dayjs().format('H');
 
+  
+  //for loop to compare current time in (24-hour format) to each time-block hour(variables assigned above)
+  //loop will run through all time-blocks from 9am to 5pm
+  //if the hour from the hourArray is equal to the current time, the time-block is assigned a class of 'present' and its styling updated to a red color
+  //if the hour from the hourArray is less than the current time, the time block is assigned a class of 'past' and its styling updated to a gray color
+  //if the hour from the hourArray is greater than the current time, the time block is assigned a class of 'future' and its styling update to a green color
   for(i=0; i<hourArray.length; i++){
 
     if (hourArray[i] == militaryTime) {
@@ -38,6 +45,7 @@
   }
 
 
+//assign variables to each save-button id element to reference later
   var Btn9El = document.getElementById('btn9');
   var Btn10El = document.getElementById("btn10");
   var Btn11El = document.getElementById("btn11");
@@ -48,93 +56,64 @@
   var Btn16El = document.getElementById("btn16");
   var Btn17El = document.getElementById("btn17");
 
-  var text9El = document.getElementById('text9');
-  var text10El = document.getElementById('text10');
-  var text11El = document.getElementById('text11');
-  var text12El = document.getElementById('text12');
-  var text13El = document.getElementById('text13');
-  var text14El = document.getElementById('text14');
-  var text15El = document.getElementById('text15');
-  var text16El = document.getElementById('text16');
-  var text17El = document.getElementById('text17');
-
-
-
+  //add eventlisteners for mouse clicks on each save-button
+  //save data entered into scheduler into local storage
   Btn9El.addEventListener("click", function(event){
     event.preventDefault();
     var text9 = document.getElementById('text9').value;
-    console.log(text9);
     localStorage.setItem("text9", JSON.stringify(text9));
   });
 
   Btn10El.addEventListener("click", function(event){
     event.preventDefault();
     var text10 = document.getElementById('text10').value;
-    console.log(text10);
     localStorage.setItem("text10", JSON.stringify(text10));
   });
 
   Btn11El.addEventListener("click", function(event){
     event.preventDefault();
     var text11 = document.getElementById('text11').value;
-    console.log(text11);
     localStorage.setItem("text11", JSON.stringify(text11));
   });
 
   Btn12El.addEventListener("click", function(event){
     event.preventDefault();
     var text12 = document.getElementById('text12').value;
-    console.log(text12);
     localStorage.setItem("text12", JSON.stringify(text12));
   });
 
   Btn13El.addEventListener("click", function(event){
     event.preventDefault();
     var text13 = document.getElementById('text13').value;
-    console.log(text13);
     localStorage.setItem("text13", JSON.stringify(text13));
   });
 
   Btn14El.addEventListener("click", function(event){
     event.preventDefault();
     var text14 = document.getElementById('text14').value;
-    console.log(text14);
     localStorage.setItem("text14", JSON.stringify(text14));
   });
-
 
   Btn15El.addEventListener("click", function(event){
     event.preventDefault();
     var text15 = document.getElementById('text15').value;
-    console.log(text15);
     localStorage.setItem("text15", JSON.stringify(text15));
   });
 
   Btn16El.addEventListener("click", function(event){
     event.preventDefault();
     var text16 = document.getElementById('text16').value;
-    console.log(text16);
     localStorage.setItem("text16", JSON.stringify(text16));
   });
 
   Btn17El.addEventListener("click", function(event){
     event.preventDefault();
     var text17 = document.getElementById('text17').value;
-    console.log(text17);
     localStorage.setItem("text17", JSON.stringify(text17));
   });
 
-  // console.log(JSON.parse(localStorage.getItem("text9")));
-  // console.log(JSON.parse(localStorage.getItem("text10")));
-  // console.log(JSON.parse(localStorage.getItem("text11")));
-  // console.log(JSON.parse(localStorage.getItem("text12")));
-  // console.log(JSON.parse(localStorage.getItem("text13")));
-  // console.log(JSON.parse(localStorage.getItem("text14")));
-  // console.log(JSON.parse(localStorage.getItem("text15")));
-  // console.log(JSON.parse(localStorage.getItem("text16")));
-  // console.log(JSON.parse(localStorage.getItem("text17")));
-
-
+  //function renderLastEvent gets time-block entry data from local storage (if there is data stored there)
+  //appends data from local storage to the page
   function renderLastEvent(){
     var lastText9 = JSON.parse(localStorage.getItem("text9"));
     if(lastText9 !== null){
@@ -198,34 +177,15 @@
     } else {
       return;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   }
 
-
-
-
-
-
-
+  //function initial() populates the time-blocks with the last saved data from local storage
   function initial(){
     renderLastEvent();
   }
+
+//calls function initial() upon browser page load and/or browser refresh
 initial();
 
-// });
-
+});
 
